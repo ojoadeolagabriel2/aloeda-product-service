400918c34b86861b9821dc554df57e6c90a8b1b2
Adeola <ojoadeolagabriel@gmail.com>
2021-05-31 14:49:56 +0100

    3rd blood - 1

--- deployment/scripts/publish_to_kubernetes.sh
+++ deployment/scripts/publish_to_kubernetes.sh
@@ -2,7 +2,9 @@
 
 set -xe
 
+# shellcheck disable=SC2039
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DEFAULT_NS="${ENV_DEFAULT_NS:-ps-ns}"
 
-kubectl delete namespace ps-ns || :
+kubectl delete namespace "${DEFAULT_NS}"|| :
 kubectl apply -f "${DIR}/../k8s/apply.yml"
---
+++ src/main/java/com/aloeda/product/api/resource/ProductResource.java
@@ -0,0 +1,11 @@
+package com.aloeda.product.api.resource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/product")
+public class ProductResource {
+}
